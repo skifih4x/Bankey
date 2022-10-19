965436febe9e20c51bffe8338e6a3cf79a2aadc9
artem <arte@kb.ri>
2022-10-19 14:21:43 +0300

    feat: add tableviewheader

---
+++ Bankey/AccountSummary/AccountSummaryHeaderVIew.swift
@@ -0,0 +1,42 @@
+//
+//  AccountSummaryHeaderVIew.swift
+//  Bankey
+//
+//  Created by Артем Орлов on 19.10.2022.
+//
+
+import UIKit
+
+class AccountSummaryHeaderVIew: UIView {
+    
+    @IBOutlet var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: 144)
+    }
+    
+    private func commonInit() {
+        let bundle = Bundle(for: AccountSummaryHeaderVIew.self)
+        bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.backgroundColor = appColor
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+}
+
+
