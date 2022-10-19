48666350b13344f6fb69a51a0bc9982d7539d7df
artem <arte@kb.ri>
2022-10-19 18:11:49 +0300

    feat : Add view model

--- Bankey/AccountSummary/Cells/AccountSummaryCell.swift
+++ Bankey/AccountSummary/Cells/AccountSummaryCell.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class AccountSummaryCell: UITableViewCell {
     
+    enum AccountType: String {
+        case Banking
+        case CreditCard
+        case Investment
+    }
+    
+    struct ViewModel {
+        let accountType: AccountType
+        let accountName: String
+    }
+    
+    let viewModel: ViewModel? = nil
+    
     let typeLabel = UILabel()
     let underLineView = UIView()
     let nameLabel = UILabel()
@@ -111,5 +124,23 @@ extension AccountSummaryCell {
         rootString.append(centString)
         
         return rootString
+    }
+}
+
+extension AccountSummaryCell {
+    func configure(with vm: ViewModel) {
+        typeLabel.text = vm.accountType.rawValue
+        nameLabel.text = vm.accountName
+        switch vm.accountType {
+        case .Banking:
+            underLineView.backgroundColor = appColor
+            balanceLabel.text = "Current balance"
+        case .CreditCard:
+            underLineView.backgroundColor = .systemOrange
+            balanceLabel.text = "Current balance"
+        case .Investment:
+            underLineView.backgroundColor = .systemPurple
+            balanceLabel.text = "Value"
+        }
     }
 }
